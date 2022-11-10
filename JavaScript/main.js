f61124d8c73b7ca728108c2982201ea1e8b087b6
Aditi Sharma <99550333+aditi3004@users.noreply.github.com>
2022-11-10 22:55:14 +0530

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -41,3 +41,77 @@ console.log(person.address.city);
 
 person.email = 'aditi.sharma';
 console.log(person.email);
+
+
+//for loop
+for (let i = 0; i < 10; i++){
+    console.log('for loop number for this iteration is :'+i);
+}
+
+//while loop
+let i = 0
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+//looping in arry
+/*for (let persons of person) {
+    console.log(persons.address);
+}*/
+
+//above one not working
+
+//forEach,map,filter
+//these are high order array methods
+const todos = [
+    {
+        id: 1,
+        text:'meeting1',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text:'meeting2',
+        isCompleted: false
+    },
+    {
+        id: 3,
+        text:'meeting3',
+        isCompleted: true
+    }
+];
+
+todos.forEach(function (todo) {
+    console.log(todo.text);
+});
+
+//using map returns an array
+const todoText = todos.map(function (todo) { 
+    return todo.text;
+});
+
+console.log(todoText);
+
+const todoCompleted = todos.filter(function (todo) { 
+    return todo.isCompleted===true;
+}).map(function (todo) {
+    return todo.text; //this retiurn the array of the task that are completed because
+    //we gave the condition in isCompleted===true so!!
+    //VVVV.IMP: JS IS FUNCTIONAL PROGRAMMING LANG.
+});
+
+console.log(todoCompleted);
+
+//conditionals
+const x = 10;
+const y='10'
+if (x == y) {
+    console.log('x is y')
+}//double equal doesnot compare the datatyes like it will string 10=int 10
+
+if (x === y) {
+    console.log('x is 10')
+} else {
+    console.log('not true')
+}
